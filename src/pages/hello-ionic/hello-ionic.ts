e70daac6b4c51499e15bfa04d3c53573faf657b9
Haydar Ali Ismail <haydar_ai@yahoo.com>
2018-08-22 16:42:08 +0800

    Add siri shortcuts to project

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { SiriShortcuts } from '@ionic-native/siri-shortcuts';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor(private siriShortcuts: SiriShortcuts) {
+  }
 
+  donatingShortcut() {
+    console.log(this.siriShortcuts);
+    this.siriShortcuts.donate({
+      persistentIdentifier: 'open-my-app',
+      title: 'Open my app',
+      suggestedInvocationPhrase: 'Open my app',
+      userInfo: { username: 'username' },
+      isEligibleForSearch: true,
+      isEligibleForPrediction: true,
+  })
+  .then(() => console.log('Shortcut donated.'))
+  .catch((error: any) => console.error(error));
   }
 }
