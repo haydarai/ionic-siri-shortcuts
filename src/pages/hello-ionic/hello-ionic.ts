094a584c5531a0112a968ea877f2e100dec554ef
Haydar Ali Ismail <haydar_ai@yahoo.com>
2018-08-28 11:28:43 +0800

    Add more button

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -10,7 +10,6 @@ export class HelloIonicPage {
   }
 
   donatingShortcut() {
-    console.log(this.siriShortcuts);
     this.siriShortcuts.donate({
       persistentIdentifier: 'open-my-app',
       title: 'Open my app',
@@ -18,8 +17,31 @@ export class HelloIonicPage {
       userInfo: { username: 'username' },
       isEligibleForSearch: true,
       isEligibleForPrediction: true,
-  })
-  .then(() => console.log('Shortcut donated.'))
-  .catch((error: any) => console.error(error));
+    })
+      .then(() => console.log('Shortcut donated.'))
+      .catch((error: any) => console.error(error));
+  }
+
+  presentShortcut() {
+    this.siriShortcuts.present({
+      persistentIdentifier: 'open-my-app',
+      title: 'Open my app',
+      suggestedInvocationPhrase: 'Open my app',
+      userInfo: { username: 'username' },
+    })
+      .then(() => console.log('Shortcut added.'))
+      .catch((error: any) => console.error(error));
+  }
+
+  removeShortcut() {
+    this.siriShortcuts.remove('open-my-app')
+      .then(() => console.log('Shortcut removed.'))
+      .catch((error: any) => console.error(error));
+  }
+
+  removeAllShortcuts() {
+    this.siriShortcuts.removeAll()
+      .then(() => console.log('All shortcuts removed removed.'))
+      .catch((error: any) => console.error(error));
   }
 }
